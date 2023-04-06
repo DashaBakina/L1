6d39e46fb8e6050c0134216cc06189094212d076
ДашаБакина <124893211+DashaBakina@users.noreply.github.com>
2023-04-06 14:06:52 +0300

    Add files via upload

---
+++ 4lb/4lb.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+extern "C" float funcasm(int x, int y);
+
+int main()
+{
+    int x, y;
+
+   /* cout << "Input x: " << endl;
+    cin >> x;
+    cout << "Input y: " << endl;
+    cin >> y;*/
+
+    x = 0;
+    y = 2;
+
+    float R = funcasm(x,y);
+          
+    cout << "Result: " << R << endl;
+    return 0;
+
+}
+
